137a01a0fc084d10108979bd6feccaa2b6263a65
Khush5476 <168684836+Khush5476@users.noreply.github.com>
2024-06-05 22:26:03 -0400

    Add files via upload

--- RPS.js
+++ RPS.js
@@ -2,8 +2,8 @@ const choices = ["ROCK", "PAPER", "SCISSORS"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay")
-const playerScoreDisplay = document.getElementById("playerScoreDisplay")
-const computerScoreDisplay = document.getElementById("computerScoreDisplay")
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 let playerScore = 0;
 let computerScore = 0;
 
